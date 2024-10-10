fe8f4298380b66191bfa69b6d2a97eeb459b8c2d
Flashut <139075536+Flashut@users.noreply.github.com>
2024-10-10 17:50:37 +0200

    Update Ejercicio4.java

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -7,7 +7,6 @@ public class Ejercicio4 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-//		Scanner sc1 = new Scanner(System.in);
 
 		Integer favnumber;
 		String favcity;
@@ -25,14 +24,8 @@ public class Ejercicio4 {
 		System.out.println("Entonces tu numero favorito es " + favnumber);
 		System.out.println("y tu cuidad favorita es " + favcity);
 
-//		System.out.println("Ingresa tu numero favorito y a continuacion tu ciudad favorita:");
-//		favnumber = sc.nextInt();
-//		favcity = sc1.nextstring();
-//
-//		System.out.println("Entonces tu numero favortio es " + favnumber + "\nY tu ciudad favorita es: " + favcity);
 
 		sc.close();
-//		sc1.close();
 
 	}
 
